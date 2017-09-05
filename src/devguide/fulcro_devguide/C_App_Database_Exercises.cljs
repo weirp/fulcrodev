d696ea8f3236ce018cc00699caa95c578a5c1419
Philip Weir <philip.weir@waivcard.com>
2017-09-05 16:45:32 +0800

    done first db exercise

--- src/devguide/fulcro_devguide/C_App_Database_Exercises.cljs
+++ src/devguide/fulcro_devguide/C_App_Database_Exercises.cljs
@@ -6,9 +6,12 @@
             [cljs.reader :as r]))
 
 (def cars-table
-  {
-                                        ; TODO (exercise 1): Add a :cars/by-id table
-   })
+  {:cars/by-id
+   {1 {:id 1 :make "Nissan" :model "Leaf"}
+    2 {:id 2 :make "Dodge" :model "Dart" }
+    3 {:id 3 :make "Ford" :model "Mustang" }}
+   }
+  )
 
 (def favorites
                                         ; TODO (exercise 2): merge your `cars-table` from above here
